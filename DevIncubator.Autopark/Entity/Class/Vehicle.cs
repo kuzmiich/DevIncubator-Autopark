50cd8d23675aeb079fc98a835355dfb4ba4d2e9c
иван кузьмич <ivankuzmic210@gmail.com>
2021-07-06 21:06:44 +0300

    fix after merge

--- DevIncubator.Autopark/Entity/Class/Vehicle.cs
+++ DevIncubator.Autopark/Entity/Class/Vehicle.cs
@@ -13,19 +13,16 @@ namespace DevIncubator.Autopark.Entity.Class
         public Vehicle()
         {
         }
-<<<<<<< HEAD
-        public Vehicle(VehicleType vehicleType, Engine vehicleEngine, string modelName, string registrationNumber, int weight, int releaseYear, int mileage, ColorType colorType, double tankCapacity = 0d)
-=======
 
         public Vehicle(VehicleType vehicleType,
+            Engine vehicleEngine,
             string modelName,
             string registrationNumber,
             int weight,
             int releaseYear,
             int mileage,
             ColorType colorType,
-            double tankCapacity = 0.0)
->>>>>>> master
+            double tankCapacity)
         {
             VehicleType = vehicleType;
             VehicleEngine = vehicleEngine;
@@ -49,7 +46,7 @@ namespace DevIncubator.Autopark.Entity.Class
         public ColorType ColorType { get; set; }
         public double TankCapacity { get; private set; }
 
-<<<<<<< HEAD
+        #endregion
 
         public decimal GetCalcTaxPerMonth => (Weight * 0.0013m) + (VehicleEngine.TaxCoefficient * VehicleType.TaxCoefficient * 30m) + 5;
 
@@ -65,26 +62,9 @@ namespace DevIncubator.Autopark.Entity.Class
             return obj is Vehicle vehicle && (VehicleType == vehicle.VehicleType && ModelName == vehicle.ModelName);
         }
 
-        public override string ToString() => $"{VehicleType}, {VehicleEngine}, Model name - {ModelName}, State number - {RegistrationNumber}, Weight - {Weight}, " +
-                   $"Release Year - {ReleaseYear}, Mileage - {Mileage}, Color type - {ColorType}, Tank capacity - {TankCapacity:0.00}, " +
-=======
-        #endregion
+        public override string ToString() =>
+            $"{VehicleType}, {VehicleEngine}, Model name - {ModelName}, State number - {RegistrationNumber}, Weight - {Weight}, " +
+            $"Release Year - {ReleaseYear}, Mileage - {Mileage}, Color type - {ColorType}, Tank capacity - {TankCapacity:0.00}, ";
 
-        public decimal GetCalcTaxPerMonth => (Weight * 0.0013m) + (VehicleType.TaxCoefficient * 30m) + 5;
-
-        public int CompareTo(Vehicle vehicle)
-        {
-            if (vehicle is null)
-            {
-                throw new ArgumentNullException(nameof(vehicle),"Error, argument can`t be null");
-            }
-
-            return vehicle.GetCalcTaxPerMonth.CompareTo(GetCalcTaxPerMonth);
-        }
-
-        public override string ToString() => $"{VehicleType}, Model name - {ModelName}, State number - {RegistrationNumber}, Weight - {Weight}, " +
-                   $"Release Year - {ReleaseYear}, Mileage - {Mileage}, Color type - {ColorType}, Tank capacity - {TankCapacity}, " +
->>>>>>> master
-                   $"Sum per month - {GetCalcTaxPerMonth:0.00}";
     }
 }
--- DevIncubator.Autopark/Entity/Class/VehicleType.cs
+++ DevIncubator.Autopark/Entity/Class/VehicleType.cs
@@ -19,16 +19,9 @@
         public virtual decimal TaxCoefficient { get; set; }
 
         #endregion
-
-<<<<<<< HEAD
+        
         public virtual string Display() => $"Type Name - {TypeName}\nTax Coefficient - {TaxCoefficient:0.00}\n";
 
-        public override string ToString() => $"Type Name - {TypeName}, Tax Coefficient - {TaxCoefficient:0.00}";
-        
-=======
-        public virtual string Display() => $"Type Name - {TypeName}\nTax Coefficient - {TaxCoefficient}\n";
-
         public override string ToString() => $"Type Name - {TypeName}, Tax Coefficient - {TaxCoefficient}";
->>>>>>> master
     }
 }
