fc1a0d11ead4119408eae5c94adee271ac957616
иван кузьмич <ivankuzmic210@gmail.com>
2021-07-06 22:29:42 +0300

    remove unnecessary using

--- DevIncubator.Autopark/Entity/Class/Vehicle.cs
+++ DevIncubator.Autopark/Entity/Class/Vehicle.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DevIncubator.Autopark.Entity.Class.VehicleComponent.Base;
 using DevIncubator.Autopark.Entity.Class.VehicleComponent.Engines.Base;
 using DevIncubator.Autopark.Entity.Enum;
 
