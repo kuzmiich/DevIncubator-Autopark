e1b40af4cb554e5957d2c9e099ef8ee787149527
иван кузьмич <ivankuzmic210@gmail.com>
2021-07-19 22:52:20 +0300

    Refactor my stack

--- DevIncubator.Autopark/Entity/Class/MyCollections/MyStack.cs
+++ DevIncubator.Autopark/Entity/Class/MyCollections/MyStack.cs
@@ -1,29 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevIncubator.Autopark.Entity.Class.MyCollections
 {
     public class MyStack<T> : IEnumerable<T>
     {
-        private T[] _array;
-        private int _size;
-
+        private readonly T[] _array;
         private const int DefaultCapacity = 10;
 
         public MyStack()
         {
             _array = new T [DefaultCapacity];
         }
-
-        // Create a stack with a specific initial count.  The initial count
-        // must be a non-negative number.
+        
         public MyStack(int count)
         {
             if (count < 0)
@@ -31,27 +21,31 @@ namespace DevIncubator.Autopark.Entity.Class.MyCollections
 
             _array = new T[count];
         }
-
-        // Fills a MyStack with the contents of a particular collection.  The items are
-        // pushed onto the stack in the same order they are read by the enumerator.
+        
         public MyStack(IEnumerable<T> collection)
         {
             if (collection is null)
-                throw new ArgumentNullException(nameof(collection));
+                throw new ArgumentNullException(nameof(collection), "Error, collection can`t be null");
 
-            _array = collection.ToArray();
+            var i = 0;
+            foreach (var element in collection)
+            {
+                _array[i] = element;
+                i++;
+            }
+
         }
-        public int Count => _size;
-        
-        // Removes all Objects from the MyStack.
+
+        public int Count => _array.Length;
+
         public void Clear()
         {
-            Array.Clear(_array, 0, _size);
-            _size = 0;
+            Array.Clear(_array, 0, Count);
         }
+
         public T Pop()
         {
-            var size = _size - 1;
+            var size = Count - 1;
 
             if ((uint)size >= (uint)_array.Length)
             {
@@ -60,17 +54,15 @@ namespace DevIncubator.Autopark.Entity.Class.MyCollections
             
             return _array[size];
         }
-
-        // Pushes an item to the top of the stack.
+        
         public void Push(T item)
         {
-            var size = _size;
+            var size = Count;
             var array = _array;
 
             if ((uint)size < (uint)array.Length)
             {
                 array[size] = item;
-                _size = size + 1;
             }
         }
 
